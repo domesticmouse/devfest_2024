1d4b1441f0b1808a446b6d3f7cdf6060483c9cad
Brett Morgan <brettmorgan@google.com>
2024-10-04 09:58:26 +1000

    Add flutter_gpu stuff

--- my_first_triangle/test/widget_test.dart
+++ my_first_triangle/test/widget_test.dart
@@ -1,16 +1,13 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+// Copyright 2024 The Flutter Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:my_first_triangle/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MainApp());
   });
